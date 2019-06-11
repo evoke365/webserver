7625e029509ded06515c621fd8f6f0c000fc077f
jgao <aus.jacy@gmail.com>
2019-06-11 23:30:43 +1000

    implemented InsertUser in mongodb

--- mongodb.go
+++ mongodb.go
@@ -1,9 +1,14 @@
 package auth
 
 import (
+	"time"
+
+	"github.com/google/uuid"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
+
+const defaultExpirySec = 60 * 60 * 24
 
 type MongoDb struct {
 	session    *mgo.Session
@@ -45,4 +50,32 @@ func (db *MongoDb) Upsert(key string, value interface{}, expiry uint32) (uint64,
 		return nil
 	})
 	return 0, nil
+}
+
+func (db *MongoDb) InsertUser(user *User) (string, error) {
+	now := time.Now()
+	tok := newToken()
+	if err := db.withCollection(func(c *mgo.Collection) error {
+		if err := c.Insert(
+			bson.M{
+				"_id":         bson.NewObjectId(),
+				"email":       user.Email,
+				"password":    user.Password,
+				"token":       tok,
+				"tokenExpiry": now.Add(time.Second * defaultExpirySec),
+				"timezone":    user.Timezone,
+				"created":     now,
+				"modified":    now,
+			}); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return "", err
+	}
+	return tok, nil
+}
+
+func newToken() string {
+	return uuid.New().String()
 }
