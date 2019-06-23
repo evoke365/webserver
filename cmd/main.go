72b5bbcedee705da12dc87e9cce7ae2381f53375
jgao <aus.jacy@gmail.com>
2019-06-23 22:50:40 +1000

    added sample cmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,30 @@
 package main
 
-import "github.com/studybox/auth"
+import (
+	"github.com/jacygao/mail"
+	"github.com/studybox/auth"
+)
+
+type fakeMailer struct {
+}
+
+func newFakeMailer() *fakeMailer {
+	return &fakeMailer{}
+}
+
+func (fm *fakeMailer) Send(to string, msg mail.Message) error {
+	return nil
+}
 
 func main() {
 	conf := auth.Config{
-		HttpPort: 8090,
+		HTTPPort:    8090,
+		RedirectURI: "https://google.com",
 	}
-	service := auth.NewService(conf)
+	service, err := auth.NewService(conf).WithMailer(newFakeMailer()).WithMemoryDB()
+	if err != nil {
+		panic(err)
+	}
+
+	service.Start()
 }
