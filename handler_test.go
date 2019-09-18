05bbeb5a63f2902b636feb31b0854b4a8a9c7c45
Jacy Gao <aus.jacy@gmail.com>
2019-09-18 23:40:18 +1000

    increase handler code coverage to 6.1%

--- handler_test.go
+++ handler_test.go
@@ -21,12 +21,36 @@ func TestHealth(t *testing.T) {
 	router.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
-		t.Fatal("incorrect status")
+		t.Fatal("non 200 status")
+	}
+}
+
+func TestUser200(t *testing.T) {
+	c := Config{1, 2}
+	testHandler := NewHandler(c, &noopModel{}, &noopCallback{})
+
+	router := httprouter.New()
+	router.GET("/user/find/:id", testHandler.User)
+
+	req, _ := http.NewRequest("GET", "/user/find/123", nil)
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Fatal("non 200 status")
 	}
 }
 
 type noopModel struct {
 	Model
+}
+
+func (m *noopModel) GetUser(id string, user *User) error {
+	user = &User{
+		Email:    "test@test.com",
+		Password: "test-password",
+	}
+	return nil
 }
 
 type noopCallback struct {
