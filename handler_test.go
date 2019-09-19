865143d8c28ae1cfdcfb94d12ccca1b367cdda57
Jacy Gao <aus.jacy@gmail.com>
2019-09-19 23:30:04 +1000

    increased to 16.4% test coverage for handler

--- handler_test.go
+++ handler_test.go
@@ -1,9 +1,11 @@
 package auth
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -11,7 +13,7 @@ import (
 // TODO: implement unit test
 func TestHealth(t *testing.T) {
 	c := Config{1, 2}
-	testHandler := NewHandler(c, &noopModel{}, &noopCallback{})
+	testHandler := NewHandler(c, &model200{}, &noopCallback{})
 
 	router := httprouter.New()
 	router.GET("/health", testHandler.Health)
@@ -27,7 +29,7 @@ func TestHealth(t *testing.T) {
 
 func TestUser200(t *testing.T) {
 	c := Config{1, 2}
-	testHandler := NewHandler(c, &noopModel{}, &noopCallback{})
+	testHandler := NewHandler(c, &model200{}, &noopCallback{})
 
 	router := httprouter.New()
 	router.GET("/user/find/:id", testHandler.User)
@@ -41,16 +43,72 @@ func TestUser200(t *testing.T) {
 	}
 }
 
-type noopModel struct {
+func TestForget200(t *testing.T) {
+	c := Config{1, 2}
+	testHandler := NewHandler(c, &model200{}, &noopCallback{})
+
+	router := httprouter.New()
+	router.GET("/user/forget/:id", testHandler.Forget)
+
+	req, _ := http.NewRequest("GET", "/user/forget/123", nil)
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Fatal("non 200 status")
+	}
+}
+
+func TestUser500(t *testing.T) {
+	c := Config{1, 2}
+	testHandler := NewHandler(c, &model500{}, &noopCallback{})
+
+	router := httprouter.New()
+	router.GET("/user/find/:id", testHandler.User)
+
+	req, _ := http.NewRequest("GET", "/user/find/123", nil)
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Fatal("non 500 status")
+	}
+}
+
+type model200 struct {
 	Model
 }
 
-func (m *noopModel) GetUser(id string, user *User) error {
+func (m *model200) GetUser(id string, user *User) error {
 	user = &User{
 		Email:    "test@test.com",
 		Password: "test-password",
 	}
 	return nil
+}
+
+func (m *model200) UpdateActiveCode(id, code string, exp time.Time) (*User, error) {
+	user := &User{
+		Email:          "test@test.com",
+		Password:       "test-password",
+		ActivationCode: "123",
+	}
+	return user, nil
+}
+
+type model500 struct {
+	Model
+}
+
+var errNotFound = errors.New("not found")
+var errMock = errors.New("mock error")
+
+func (m *model500) GetUser(id string, user *User) error {
+	return errMock
+}
+
+func (m model500) IsErrNotFound(err error) bool {
+	return err == errNotFound
 }
 
 type noopCallback struct {
