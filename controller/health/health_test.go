8520c75116963b30ed87dc30b46f733b21323633
jacygao <aus.jacy@gmail.com>
2020-07-20 18:08:18 +1000

    added a unit test for healthz

--- controller/health/health_test.go
+++ controller/health/health_test.go
@@ -1,1 +1,15 @@
 package health
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestHealthz(t *testing.T) {
+	testController := NewController()
+	responder := testController.Healthz()
+	expected := &HealthzOK{}
+	if !reflect.DeepEqual(responder, expected) {
+		t.Fatalf("results do not match! expected %+v but got %+v", expected, responder)
+	}
+}
